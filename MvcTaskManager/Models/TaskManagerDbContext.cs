9efb231d09d67264551e0c2a8b2e0750b4a4dad1
jtaptso <taptso@yahoo.fr>
2020-06-11 10:11:02 +0200

    Projektdateien hinzufügen.

---
+++ MvcTaskManager/Models/TaskManagerDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MvcTaskManager.Models
+{
+    public class TaskManagerDbContext : DbContext
+    {
+        public DbSet<Project> Projects { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("data source=TAPTSO\\JTAPTSOSQLSERVER; integrated security=yes; initial catalog=TaskManagerDatabase");
+        }
+
+    }
+
+}
